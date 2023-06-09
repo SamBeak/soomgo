148d9f5a74c7cac21c9fc5dece08a024453c85dc
Baek HyunGyu <baekhg102@gmail.com>
2023-06-10 00:21:54 +0900

    Initial Commit

---
+++ src/components/wrap/user/JoinComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JoinComponent() {
+  return (
+    <div id="join">
+        <p>Join</p>
+    </div>
+  )
+}
---
+++ src/components/wrap/user/LoginComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginComponent() {
+  return (
+    <div id="login">
+        <p>Login</p>
+    </div>
+  )
+}
