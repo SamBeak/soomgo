098e7dec575ca4d5d75f6b886f3a3c4bc7ad33ca
Hyungyu <whdmsspt@naver.com>
2023-07-05 15:51:19 +0900

    docs: Add Section2Component.jsx to #home

--- src/components/wrap/HomeComponent.jsx
+++ src/components/wrap/HomeComponent.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Section1Component from './home/Section1Component'
+import Section2Component from './home/Section2Component'
 
 export default function HomeComponent() {
   return (
-    <Section1Component />
+    <div id="home">
+      <Section1Component />
+      <Section2Component />
+    </div>
   )
 }
