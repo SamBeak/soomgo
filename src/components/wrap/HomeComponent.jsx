b9a3ed2ef773211177f333227570d396b19da082
Hyungyu <whdmsspt@naver.com>
2023-07-06 17:49:38 +0900

    docs: Add section6Component.jsx

--- src/components/wrap/HomeComponent.jsx
+++ src/components/wrap/HomeComponent.jsx
@@ -4,6 +4,7 @@ import Section2Component from './home/Section2Component'
 import Section3Component from './home/Section3Component'
 import Section4Component from './home/Section4Component'
 import Section5Component from './home/Section5Component'
+import Section6Component from './home/Section6Component'
 
 export default function HomeComponent() {
   return (
@@ -13,6 +14,7 @@ export default function HomeComponent() {
       <Section3Component />
       <Section4Component />
       <Section5Component />
+      <Section6Component />
     </div>
   )
 }
