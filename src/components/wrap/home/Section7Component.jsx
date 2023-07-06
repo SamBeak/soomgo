f60927fb9da5ef2d856afe9b3e8d408d06ecdd9c
Hyungyu <whdmsspt@naver.com>
2023-07-06 18:12:31 +0900

    docs: Add section7component.jsx

---
+++ src/components/wrap/home/Section7Component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Section7Component() {
+  return (
+    <section id="section7">
+        <div className="container">
+            <div className="title">
+                <h2>숨은고수 포트폴리오</h2>
+                <a href="!#" className='show__all'>
+                    <span>전체보기</span>
+                    <img src="./images/chev-right.svg" alt="오른쪽 화살표" />
+                </a>
+            </div>
+            <div className="content">
+            </div>
+        </div>
+    </section>
+  )
+}
